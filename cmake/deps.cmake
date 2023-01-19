ac575d3d305e6066dd56e2fc00627c52d6ba088e
Paul Walker <soccer-belarus@mail.ru>
2023-01-19 23:53:18 +0300

    Init

---
+++ cmake/deps.cmake
@@ -0,0 +1,67 @@
+
+set(CAPSTONE_URL
+    "https://github.com/capstone-engine/capstone/archive/refs/tags/5.0-rc2.zip"
+    CACHE STRING "URL of Capstone archive to use."
+)
+set(CAPSTONE_ARCHIVE_SHA256
+    "c47acdabb9ba4922a6d68b96eb7e14a431bfef7d7c57cea1e5881f87776228b2"
+    CACHE STRING ""
+)
+
+set(GOOGLETEST_URL
+    "https://github.com/google/googletest/archive/90a443f9c2437ca8a682a1ac625eba64e1d74a8a.zip"
+    CACHE STRING "URL of Googletest archive to use."
+)
+set(GOOGLETEST_ARCHIVE_SHA256
+    "6fb9a49ad77656c860cfdafbb3148a91f076a3a8bda9c6d8809075c832549dd4"
+    CACHE STRING ""
+)
+
+set(KEYSTONE_URL
+    "https://github.com/keystone-engine/keystone/archive/d7ba8e378e5284e6384fc9ecd660ed5f6532e922.zip"
+    CACHE STRING "URL of Keystone archive to use."
+)
+set(KEYSTONE_ARCHIVE_SHA256
+    "13bd00e062e9c778fe76aaab5c163348b3c9457c0e9b2a4c2fb3e2d8747694ca"
+    CACHE STRING ""
+)
+
+set(LLVM_URL
+    "https://github.com/avast/llvm/archive/2a1f3d8a97241c6e91710be8f84cf3cf80c03390.zip"
+    CACHE STRING "URL of LLVM archive to use."
+)
+set(LLVM_ARCHIVE_SHA256
+    "96389d211d74e758ae35957e1fc7366b65edbd3d2342badc9800a8be8bae1cb8"
+    CACHE STRING ""
+)
+
+set(YARA_URL
+    "https://github.com/VirusTotal/yara/archive/v4.2.0-rc1.zip"
+    CACHE STRING "URL of Yara archive to use."
+)
+set(YARA_ARCHIVE_SHA256
+    "ae1adad2ae33106f4c296cef32ddba2c93867010ef853028d30cad42548d0474"
+    CACHE STRING ""
+)
+
+set(YARAMOD_URL
+    "https://github.com/avast/yaramod/archive/a367d910ae79698e64e99d8414695281723cd34b.zip"
+    CACHE STRING "URL of YaraMod archive to use."
+)
+set(YARAMOD_ARCHIVE_SHA256
+    "2d2dc60890ad9b796fb908162bd3968b8ada693ab2b08e55cdd1c359c643b72b"
+    CACHE STRING ""
+)
+
+set(SUPPORT_PKG_URL
+    "https://github.com/avast/retdec-support/releases/download/2019-03-08/retdec-support_2019-03-08.tar.xz"
+    CACHE STRING "URL of RetDec support package to use."
+)
+set(SUPPORT_PKG_SHA256
+    "629351609bca0f4b8edbd4e53789192305256aeb908e953f5546e121a911d54e"
+    CACHE STRING ""
+)
+set(SUPPORT_PKG_VERSION
+    "2019-03-08"
+    CACHE STRING ""
+)
